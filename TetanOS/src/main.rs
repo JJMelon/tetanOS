33b77b633f1b2921bd05dc331b06cdae96851f2e
Makoto Tomokiyo <mtomokiy@andrew.cmu.edu>
2022-03-17 18:29:46 -0400

    finished vga

--- TetanOS/src/main.rs
+++ TetanOS/src/main.rs
@@ -8,13 +8,16 @@ mod vga_buffer;
 // Taken from https://os.phil-opp.com/minimal-rust-kernel/#building-our-kernel
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_hello_msg();
+    println!("Hello World{}", "!");
+    panic!("message here");
 
     loop {}
 }
 
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
     loop{}
 }
--- TetanOS/src/vga_buffer.rs
+++ TetanOS/src/vga_buffer.rs
@@ -1,7 +1,16 @@
 // Derived from https://os.phil-opp.com/vga-text-mode/
 use volatile::Volatile;
 use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
 
+lazy_static! {
+  pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+      column_position: 0,
+      color_code: ColorCode::new(Color::Yellow, Color::Black),
+      buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+  });
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -123,18 +132,19 @@ impl fmt::Write for Writer {
   }
 }
 
-/*
- * Example function utilizing buffers
-*/
-pub fn print_hello_msg() {
-  use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
 
-    writer.write_byte(b'H');
-    writer.write_string("ello! ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
