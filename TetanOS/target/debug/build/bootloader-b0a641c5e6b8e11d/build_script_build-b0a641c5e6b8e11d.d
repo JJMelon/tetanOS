7bb61daeb9bd342f159b5f0ec0f007cb060ce9f8
Makoto Tomokiyo <mtomokiy@andrew.cmu.edu>
2022-04-02 12:44:27 -0400

    added serial port interface

---
+++ TetanOS/target/debug/build/bootloader-b0a641c5e6b8e11d/build_script_build-b0a641c5e6b8e11d.d
@@ -0,0 +1,5 @@
+/Users/makototomokiyo/dev/tetanOS/TetanOS/target/debug/build/bootloader-b0a641c5e6b8e11d/build_script_build-b0a641c5e6b8e11d: /Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.22/build.rs
+
+/Users/makototomokiyo/dev/tetanOS/TetanOS/target/debug/build/bootloader-b0a641c5e6b8e11d/build_script_build-b0a641c5e6b8e11d.d: /Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.22/build.rs
+
+/Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.22/build.rs:
---
+++ TetanOS/target/debug/build/rustversion-19a91c9281db177e/build_script_build-19a91c9281db177e.d
@@ -0,0 +1,6 @@
+/Users/makototomokiyo/dev/tetanOS/TetanOS/target/debug/build/rustversion-19a91c9281db177e/build_script_build-19a91c9281db177e: /Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/rustversion-1.0.6/build/build.rs /Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/rustversion-1.0.6/build/rustc.rs
+
+/Users/makototomokiyo/dev/tetanOS/TetanOS/target/debug/build/rustversion-19a91c9281db177e/build_script_build-19a91c9281db177e.d: /Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/rustversion-1.0.6/build/build.rs /Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/rustversion-1.0.6/build/rustc.rs
+
+/Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/rustversion-1.0.6/build/build.rs:
+/Users/makototomokiyo/.cargo/registry/src/github.com-1ecc6299db9ec823/rustversion-1.0.6/build/rustc.rs:
