dca4edd3676b6c39f2fdf8d475e479264b0188da
David Barajas <dbarajas@andrew.cmu.edu>
2022-04-16 14:34:56 -0400

    h

--- TetanOS/src/main.rs
+++ TetanOS/src/main.rs
@@ -5,26 +5,8 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-<<<<<<< HEAD
     println!("Hello World{}", "!");
     loop {}
-=======
-    loop {
-        print!("> ");
-
-        let mut input = readline();
-        let command = tokenize(input);
-            match command {
-                exit => {
-                    println!("Goodbye");
-                },
-                none => {
-                    println!("not implemented");
-                },
-            }
-        }
-    }
->>>>>>> main
 }
 
 #[panic_handler]
