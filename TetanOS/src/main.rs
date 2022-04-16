95b6edb382fbbf27b1107a5dd1e0b08b0ba8a347
David Barajas <dbarajas@andrew.cmu.edu>
2022-04-16 15:11:23 -0400

    attempt at reading from keyboard

--- TetanOS/src/main.rs
+++ TetanOS/src/main.rs
@@ -2,12 +2,26 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::arch::asm;
+
 mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    loop {}
+    loop {
+        unsafe {
+            asm!(
+                "mov ah, 0x00",
+                "2:",
+                "mov ah,0x00",
+                "int 0x16",
+                "mov ah,0x0e",
+                "int 0x10",
+                "jmp 2b",
+            );
+        }
+    }
 }
 
 #[panic_handler]
