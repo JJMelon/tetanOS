27e515bdd9a8a2b7bce323b70ac11f52361e1858
David Barajas <dbarajas@andrew.cmu.edu>
2022-04-16 14:33:40 -0400

    anew

--- TetanOS/src/main.rs
+++ TetanOS/src/main.rs
@@ -5,6 +5,10 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+<<<<<<< HEAD
+    println!("Hello World{}", "!");
+    loop {}
+=======
     loop {
         print!("> ");
 
@@ -20,6 +24,7 @@ pub extern "C" fn _start() -> ! {
             }
         }
     }
+>>>>>>> main
 }
 
 #[panic_handler]
